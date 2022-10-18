e3e2475e81eca44a48d19c338a3123f2e850587f
Martha Nieto <m27842n@outlook.com>
2022-10-17 21:45:16 -0700

    - update: main - added constructor property promotion, change language.

--- classes/Email.php
+++ classes/Email.php
@@ -4,20 +4,18 @@ namespace Classes;
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-class Email {
-
-    public $email;
-    public $nombre;
-    public $token;
+class Email 
+{
     
-    public function __construct($email, $nombre, $token)
-    {
-        $this->email = $email;
-        $this->nombre = $nombre;
-        $this->token = $token;
-    }
+    public function __construct(
+        public string $email, 
+        public string $first_name, 
+        public string $last_name, 
+        public string $token)
+    {}
 
-    public function enviarConfirmacion() {
+    public function sendConfirmation() 
+    {
 
          // create a new object
          $mail = new PHPMailer();
@@ -28,27 +26,26 @@ class Email {
          $mail->Username = $_ENV['EMAIL_USER'];
          $mail->Password = $_ENV['EMAIL_PASS'];
      
-         $mail->setFrom('cuentas@tjdevwebconf.com');
-         $mail->addAddress($this->email, $this->nombre);
-         $mail->Subject = 'Confirma tu Cuenta';
+         $mail->setFrom('account@tjdevwebconf.com');
+         $mail->addAddress($this->email, $this->first_name, $this->last_name);
+         $mail->Subject = 'Confirm your account';
 
          // Set HTML
          $mail->isHTML(TRUE);
          $mail->CharSet = 'UTF-8';
 
-         $contenido = '<html>';
-         $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has Registrado Correctamente tu cuenta en TJDevWebConf; pero es necesario confirmarla</p>";
-         $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a>";       
-         $contenido .= "<p>Si tu no creaste esta cuenta; puedes ignorar el mensaje</p>";
-         $contenido .= '</html>';
-         $mail->Body = $contenido;
+         $content = '<html>';
+         $content .= "<p><strong>Hi " .$this->first_name. " " .$this->last_name.  "</strong> Your TJDevWebConf account has been register correctly; but we need your confirmation</p>";
+         $content .= "<p>Click Here <a href='" . $_ENV['HOST'] . "/confirm-account?token=" . $this->token . "'>Confirm your Account</a>";       
+         $content .= "<p>If you didn't request this confirmation you can ignore it.</p>";
+         $content .= '</html>';
+         $mail->Body = $content;
 
-         //Enviar el mail
          $mail->send();
 
     }
 
-    public function enviarInstrucciones() {
+    public function sendInstructions() {
 
         // create a new object
         $mail = new PHPMailer();
@@ -59,22 +56,21 @@ class Email {
         $mail->Username = $_ENV['EMAIL_USER'];
         $mail->Password = $_ENV['EMAIL_PASS'];
     
-        $mail->setFrom('cuentas@tjdevwebconf.com');
-        $mail->addAddress($this->email, $this->nombre);
-        $mail->Subject = 'Reestablece tu password';
+        $mail->setFrom('account@tjdevwebconf.com');
+        $mail->addAddress($this->email, $this->first_name, $this->last_name);
+        $mail->Subject = 'Recover your password';
 
         // Set HTML
         $mail->isHTML(TRUE);
         $mail->CharSet = 'UTF-8';
 
-        $contenido = '<html>';
-        $contenido .= "<p><strong>Hola " . $this->nombre .  "</strong> Has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido .= "<p>Presiona aquí: <a href='" . $_ENV['HOST'] . "/recuperar?token=" . $this->token . "'>Reestablecer Password</a>";        
-        $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
-        $contenido .= '</html>';
-        $mail->Body = $contenido;
+        $content = '<html>';
+        $content .= "<p><strong>Hi " . $this->first_name .  "</strong> You have been recover your password, follow the next link to complete this request.</p>";
+        $content .= "<p>Click here: <a href='" . $_ENV['HOST'] . "/recover?token=" . $this->token . "'>Recover Password</a>";        
+        $content .= "<p>If you do not request this change you can ignore this message.</p>";
+        $content .= '</html>';
+        $mail->Body = $content;
 
-        //Enviar el mail
         $mail->send();
     }
 }
